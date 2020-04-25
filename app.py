43d529ed96e2125da58568b6b8e294d6064d4166
Meaghan-Boddie <boddie.meaghan@gmail.com>
2020-04-24 19:12:07 -0500

    How to Install Dependencies for Flask Database and API Features

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,14 +10,11 @@ def hello():
 
 if __name__== '__main__':
     app.run(debug=True)
-    
-
-
 
 
 # from flask import Flask, request, jsonify
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_marshmallow import Marshmallow
+
+
 # import os
 
 # app = Flask(__name__)
